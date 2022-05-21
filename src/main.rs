e816b176d05dbd30d96c0dde30b01fc5fb869baf
baol <mirko.maischberger@gmail.com>
2022-05-21 11:11:16 +0200

    Display multiple pictures in a grid

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,15 @@ struct Filenames {
     names: Vec<PathBuf>,
 }
 
+#[derive(Component)]
+struct Photo;
+
 fn main() {
     let args = Args::parse();
 
     use glob::glob;
 
-    let filenames = glob(format!("{}/**/*.jp?g", args.path).as_str())
+    let filenames = glob(format!("{}/**/*.jp*g", args.path).as_str())
         .expect("Failed to read glob pattern")
         .filter(|entry| entry.is_ok())
         .map(|entry| entry.unwrap())
@@ -31,27 +34,25 @@ fn main() {
         .insert_resource(Filenames { names: filenames })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(picture_fly)
+        .add_system(picture_grid)
         .run();
 }
 
-#[derive(Component)]
-enum Direction {
-    Up,
-    Down,
-}
-
-fn picture_fly(time: Res<Time>, mut q: Query<(&mut Transform, &mut Direction)>) {
-    for (mut transform, mut direction) in q.iter_mut() {
-        match *direction {
-            Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
-            Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
-        }
-
-        if transform.translation.y > 200. {
-            *direction = Direction::Down;
-        } else if transform.translation.y < -200. {
-            *direction = Direction::Up;
+fn picture_grid(mut q: Query<(&mut Transform, &Photo)>) {
+    let startx = -500.0;
+    let maxx = 500.0;
+    let stepx = 170.0;
+    let starty = 300.0;
+    let stepy = -110.0;
+    let mut x = startx;
+    let mut y = starty;
+    for (mut transform, _) in q.iter_mut() {
+        transform.translation.x = x;
+        transform.translation.y = y;
+        x += stepx;
+        if x > maxx {
+            x = startx;
+            y += stepy;
         }
     }
 }
@@ -65,12 +66,16 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, filenames: Res<
         .map(|name| asset_server.load(name.as_path()));
 
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands
-        .spawn_bundle(SpriteBundle {
-            // FIXME: taking just the first image for now
-            texture: images.last().take().unwrap(),
-            transform: Transform::from_scale(Vec3::splat(0.3)),
-            ..default()
-        })
-        .insert(Direction::Up);
+    images.for_each(|image: Handle<Image>| {
+        commands
+            .spawn_bundle(SpriteBundle {
+                sprite: Sprite {
+                    custom_size: Some(Vec2::new(160.0, 100.0)),
+                    ..default()
+                },
+                texture: image,
+                ..default()
+            })
+            .insert(Photo);
+    })
 }
